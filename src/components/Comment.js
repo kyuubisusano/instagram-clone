33f47f17113fb2f6ecac155ede421b5ce7a9b79a
kyuubisusano <perfectsusano123@gmail.com>
2022-06-08 02:59:11 +0530

    tweaked the styling need more work though

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,25 +1,41 @@
-import { Avatar, Box, Button } from '@mui/material'
+import { Avatar, Box, Button, IconButton, Typography } from '@mui/material'
+import { collection, doc, increment, updateDoc } from 'firebase/firestore'
 import React, { useEffect } from 'react'
 import ReactTimeAgo from 'react-time-ago'
+import LikeIcon from '../favorite_border_black.svg'
+import { db } from '../firebase'
 
-function Comment({id,comment,sender}) {
+function Comment({postId,commentId,comment,sender}) {
+  
+  const handleLikeBtn = async () => {
+    const commentRef = doc(db, "posts",`${postId}`,"comments",`${commentId}`)
+    await updateDoc(commentRef, {
+      likes: increment(1)
+  });
+
+  }
 
   if(comment.timeStamp && comment.text)  
   return (
-    <Box sx={{display: 'flex' }} className='post_comment'>
+    <div style={{display: 'flex' ,}} className='post_comment'>
         <Avatar sx={{ mr:1 ,width:20 ,height:20 }}
                 alt={sender.userName}
                 src={sender.photoUrl}
                  />
-        <Box >
-            <Box sx={{alignItems: 'center',display: 'flex' }}>
+        <div style={{width:'100%' ,maxWidth: 420, }} >
+            <div style={{ width: '100%', textOverflow: 'ellipsis', whiteSpace: "nowrap", overflow:'hidden' , }}>
                 
                <strong>{sender.userName}</strong> {comment.text}
-            </Box>
+            </div>
+            <div style={{display: 'flex', alignItems:  'center'}}>
             <ReactTimeAgo date={comment.timeStamp.toDate()} locale="en-US" timeStyle="twitter" style={{fontSize: '15px'}}/>
-            <Button>like</Button>
-        </Box>      
-    </Box>
+            <p style={{marginInlineStart: 8}}>{comment.likes} likes</p>
+            </div>
+        </div>      
+        
+          <img  style={{marginInlineStart: 5,marginTop:0, marginBottom: 'auto', marginInlineEnd:0}} src={LikeIcon} onClick={handleLikeBtn}  />
+        
+    </div>
   )
   return (<div></div>)
 }
